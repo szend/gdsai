594eb62e8f3eb01aaa5b8a54bfa911dc57d533c6
szend <55789153+szend@users.noreply.github.com>
2025-01-23 14:16:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,52 @@
-print("app")
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+
+app = FastAPI()
+
+
+MODEL_NAME = ".\\myt5small"
+
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+
+
+class InputData(BaseModel):
+
+    input_text: str
+
+
+
+@app.get("/")
+
+def root():
+
+    return {"message": "T5 API is running!"}
+
+
+@app.post("/generate/")
+
+def generate_text(data: InputData):
+
+
+    inputs = tokenizer(data.input_text, return_tensors="pt", padding=True, truncation=True)
+
+ 
+
+    outputs = model.generate(**inputs, max_length=50, num_beams=5, early_stopping=True)
+
+
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+
+    return {"generated_text": generated_text}
+
+
+
+
+
