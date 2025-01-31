b8da7cb165fbd26c7b41d291f8a1e6121587e142
szend <55789153+szend@users.noreply.github.com>
2025-01-31 10:47:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
-from fastapi import FastAPI
+import streamlit as st
 
-import logging
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
 
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
 
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
 
-app = FastAPI()
+    from flask import Flask
 
+    app = Flask(__name__)
 
+    @app.route('/foo')
+    def serve_foo():
+        return 'This page is served via Flask!'
 
-# Konfiguráld a naplózást
-
-logging.basicConfig(level=logging.INFO)
-
-logger = logging.getLogger("uvicorn.error")
-
-
-
-@app.get("/")
-
-async def root():
-
-    logger.info("Root endpoint called")
-
-    return {"message": "Hello, World"}
-
+    app.run(port=8888)
 
 
-@app.get("/error")
+# We'll never reach this part of the code the first time this file executes!
 
-async def cause_error():
-
-    logger.error("Something went wrong!")
-
-    raise RuntimeError("This is a test error")
+# Your normal Streamlit app goes here:
+x = st.slider('Pick a number')
+st.write('You picked:', x)
