b8a8966dbf44fae311c127fed4bbbf04ad54567e
szend <55789153+szend@users.noreply.github.com>
2025-01-31 10:58:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,18 @@ import streamlit as st
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 
+MODEL_NAME = "t5-small" 
 
-st.write('You picked:')
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+inputs = tokenizer(data.input_text, return_tensors="pt", padding=True, truncation=True)
+
+outputs = model.generate(**inputs, max_length=50, num_beams=5, early_stopping=True)
+
+
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+
+
+st.write('Text: ' + generated_text)
