ccd747f1a607b17facca7e80893f82e868ef9fd4
szend <55789153+szend@users.noreply.github.com>
2025-02-04 14:40:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
+MODEL_NAME = "t5-small" 
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
 
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
 app = FastAPI()
 
@@ -14,10 +17,6 @@ async def read_root():
 
 @app.get("/")
 async def read_root(): 
-    MODEL_NAME = "t5-small" 
-    tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
-
-    model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
     inputs = tokenizer("halo, are you there?", return_tensors="pt", padding=True, truncation=True)
 
     outputs = model.generate(**inputs, max_length=256, num_beams=2, early_stopping=True)
@@ -28,10 +27,6 @@ async def read_root():
 
 @app.get("/input")
 async def read_input(inputstr: str = ""): 
-    MODEL_NAME = "t5-small" 
-    tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
-
-    model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
     inputs = tokenizer(inputstr, return_tensors="pt", padding=True, truncation=True)
 
     outputs = model.generate(**inputs, max_length=256, num_beams=2, early_stopping=True)
