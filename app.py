aad998f34dfbdac03b86c5a14ec24c7b5b65cf7c
szend <55789153+szend@users.noreply.github.com>
2025-02-06 13:54:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ from fastapi import FastAPI
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME,use_fast=True, token="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")
 
 
 app = FastAPI()
 
-@app.get("/input")
-async def read_input(inputstr: str = ""): 
-    
+@app.get("/")
+async def read_input(inputstr: str = "alma"): 
+    inputs = tokenizer(inputstr, return_tensors="pt", padding=True, truncation=True)
+    torch.save(inputs, "tokenized_inputs.pt")  
+    loaded_inputs = torch.load("tokenized_inputs.pt")  
 
-    return {"generated_text": "1"}  
+
+
+    return {loaded_inputs}  
+
+
+@app.get("/input")
+async def read_input(inputstr: str = "alma"): 
+    inputs = tokenizer(inputstr, return_tensors="pt", padding=True, truncation=True)
+
+
+
+
+    return {inputs}  
