3bfb5b91765c0926e7b4ac4d20e290a586d36f62
szend <55789153+szend@users.noreply.github.com>
2025-02-06 12:32:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,12 @@ from fastapi import FastAPI
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 
-MODEL_NAME = "google/t5-efficient-mini" 
-tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME,use_fast=True, token="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")
 
-model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
 app = FastAPI()
 
 @app.get("/input")
 async def read_input(inputstr: str = ""): 
     
-    inputs = tokenizer(inputstr, return_tensors="pt", padding=True, truncation=True)
-    outputs = model.generate(**inputs, max_length=256, num_beams=1)
-    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return {"generated_text": generated_text}  
+
+    return {"generated_text": "1"}  
